e512c696464ac566c6035ef97ebf5ebcc063d276
anish jain <helloanishjain@gmail.com>
2022-05-31 19:35:35 +0530

    add IPFS metadat and chainlink for LW#

--- hardhat-smart-contracts/hardhat.config.js
+++ hardhat-smart-contracts/hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-const ALCHEMY_POLYGON_MAINNET_API_KEY_URL = process.env.ALCHEMY_POLYGON_MAINNET_API_KEY_URL;
+const HACKED_ACCOUNT_PRIVATE_KEY = process.env.HACKED_ACCOUNT_PRIVATE_KEY;
+const ALCHEMY_MUMBAI_API_KEY_URL = process.env.ALCHEMY_MUMBAI_API_KEY_URL;
 
 const POLYGON_MAINNET_PRIVATE_KEY = process.env.POLYGON_MAINNET_PRIVATE_KEY;
 const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
@@ -9,14 +10,19 @@ const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    polygon_mainnet: {
-      url: ALCHEMY_POLYGON_MAINNET_API_KEY_URL,
-      accounts: [POLYGON_MAINNET_PRIVATE_KEY],
+    mumbai: {
+      url: ALCHEMY_MUMBAI_API_KEY_URL,
+      accounts: [HACKED_ACCOUNT_PRIVATE_KEY],
     },
+
+    // polygon_mainnet: {
+    //   url: ALCHEMY_POLYGON_MAINNET_API_KEY_URL,
+    //   accounts: [POLYGON_MAINNET_PRIVATE_KEY],
+    // },
   },
   etherscan: {
     apiKey: {
-      polygon: POLYGONSCAN_KEY,
+      polygonMumbai: POLYGONSCAN_KEY,
     },
   },
 };
---
+++ hardhat-smart-contracts/scripts/NFTee_deploy.js
@@ -0,0 +1,41 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+
+async function main() {
+    /*
+    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+    so verifyContract here is a factory for instances of our Verify contract.
+    */
+    const NFTeeContract = await ethers.getContractFactory("NFTee");
+
+    // deploy the contract
+    const deployedNFTeeContract = await NFTeeContract.deploy("ipfs://bafkreidxw7o6evxxp6pfzdtreyuyai2at6ikmiaauwknmr2t5smfrifumy");
+
+    await deployedNFTeeContract.deployed();
+
+    // print the address of the deployed contract
+    console.log("NFTee Contract Address:", deployedNFTeeContract.address);
+
+    console.log("Sleeping.....");
+    // Wait for etherscan to notice that the contract has been deployed
+    await sleep(50000);
+
+    // Verify the contract after deploying
+    await hre.run("verify:verify", {
+        address: deployedNFTeeContract.address,
+        constructorArguments: ["ipfs://bafkreidxw7o6evxxp6pfzdtreyuyai2at6ikmiaauwknmr2t5smfrifumy"],
+    });
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
